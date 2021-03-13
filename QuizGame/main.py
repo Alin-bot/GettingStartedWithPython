33ae4781fd1c625eaca137cb0404664967b8f123
Vezeteu Alin <alin.vezeteu@yahoo.ro>
2021-03-13 11:24:52 +0200

    now it works

--- QuizGame/main.py
+++ QuizGame/main.py
@@ -1,22 +1,22 @@
-from Student import Question
-    question_prompt = [
+from Question import Question
+question_prompt = [
         'What color are bananas?\na) Blue\nb) Banana\nc) Yellow\n',
         'What color are kiwis?\na) Green\nb) Brown \nc) Banana\n',
-        'What color are strawberries\na) Red\nb) Green\nc) Green\n' ]
-    questions = [ Question(question_prompt[0], 'c'),
-              Question(question_prompt[1], 'b'),
-              Question(question_prompt[2], 'a') ]
+        'What color are strawberries\na) Red\nb) Green\nc) Green\n']
+questions = [
+    Question(question_prompt[0], 'c'),
+    Question(question_prompt[1], 'b'),
+    Question(question_prompt[2], 'a')]
+
 
 def run_test(questions):
+    print('Just type the letter of your answer!\n')
     score = 0
     for question in questions:
-        answer = input(question.prompt)
+        answer = input(question.prompt + 'Your answer: ')
         if answer == question.answer:
             score += 1
     print('You got ' + str(score) + '/' + str(len(questions)) + ' correct')
-    \run_test(questions)
 
-class Question:
-    def __init__(self, prompt, answer):
-        self.prompt = prompt
-        self.answer = answer
+
+run_test(questions)
